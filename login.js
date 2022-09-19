e44242a21def6c5b24edab6ddf0666b1358b17cf
DaniloG <dan_gaaspar@hotmail.com>
2022-09-19 18:50:15 -0300

    Adicionando Login.js e criando animação

---
+++ login.js
@@ -0,0 +1,6 @@
+document.querySelector("#show-login").addEventListener("click",function(){
+    document.querySelector(".blocoform").classList.add("active");
+  });
+  document.querySelector(".blocoform .close-btn").addEventListener("click",function(){
+    document.querySelector(".blocoform").classList.remove("active");
+  });
