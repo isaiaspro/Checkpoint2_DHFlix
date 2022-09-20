a5dffbf422a101706e3a619bd4a8394cdb15be07
DaniloG <dan_gaaspar@hotmail.com>
2022-09-20 17:33:13 -0300

    Adicionando Forms

---
+++ js/form.js
@@ -0,0 +1,8 @@
+import '../css/form.css';
+
+document.querySelector("#show-login").addEventListener("click",function(){
+    document.querySelector(".blocoform").classList.add("active");
+  });
+  document.querySelector(".blocoform .close-btn").addEventListener("click",function(){
+    document.querySelector(".blocoform").classList.remove("active");
+  });
